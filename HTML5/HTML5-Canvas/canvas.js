c45ae02cf2fc218dfa0d328de90fc47e071830ee
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-31 13:20:48 +0530

    Styled the rectangular boxes

--- HTML5/HTML5-Canvas/canvas.js
+++ HTML5/HTML5-Canvas/canvas.js
@@ -4,6 +4,9 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
+c.fillStyle = "rgba(0,0,255,0.6)";
 c.fillRect(100, 100, 100, 100);
+c.fillStyle = "rgba(0,255,0,0.6)";
 c.fillRect(200, 200, 100, 100);
+c.fillStyle = "rgba(255,0,255,0.6)";
 c.fillRect(300, 300, 100, 100);
